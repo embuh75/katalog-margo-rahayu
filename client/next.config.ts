3d017795458983522225350561875c9d8c6b83b4
FUN <coolbanditz75@gmail.com>
2025-08-23 00:54:11 +0700

    restore default

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
-  eslint: {
-    // ⛔ jangan gagal build walau ada error eslint
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // ⛔ jangan gagal build walau ada error typescript
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
