12ad37eeabbe41f10d66bdd5bce79f967b5472e9
FUN <coolbanditz75@gmail.com>
2025-08-23 00:46:15 +0700

    disable eslint

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
+  eslint: {
+    // ⛔ jangan gagal build walau ada error eslint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
